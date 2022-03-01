9ea74ff8d95154834265ab2c07c5dc4f2a286174
Alex-new-laptop <acontell@gmail.com>
2022-03-01 16:35:04 -0500

    testing binance API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('https://api.binance.us/api/v3/ticker/price?symbol=ETHUSD').then(
+      // fetch('https://api.binance.us/api/v3/trades?symbol=ETHUSD').then(
+      (res) => {
+        if (res.ok) {
+          res.json().then((data) => {
+            console.log(data);
+          });
+        }
+      }
+    );
+  }, []);
+
   return (
     <div className="main-container">
       <h1>crypto-dice</h1>
