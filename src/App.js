caacde985cff3d4fa9a9d7ee9e7117ab07e6bc19
Alex-new-laptop <acontell@gmail.com>
2022-03-01 18:18:51 -0500

    using binance websockets instead of API

--- src/App.js
+++ src/App.js
@@ -3,42 +3,24 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  let [coinbasePrice, setCoinbasePrice] = useState(null);
-  let [binancePrice, setBinancePrice] = useState(null);
-  useEffect(() => {
-    fetch('https://api.coinbase.com/v2/prices/spot?currency=USD').then(
-      // fetch('https://api.binance.us/api/v3/ticker/price?symbol=ETHUSD').then(
-      // fetch('https://api.binance.us/api/v3/trades?symbol=ETHUSD').then(
-      (res) => {
-        if (res.ok) {
-          res.json().then((data) => {
-            console.log('Bitcoin price on Coinbase: ' + data.data.amount);
-            setCoinbasePrice(data.data.amount);
-          });
-        }
-      }
-    );
+  let [bitcoinPrice, setBitcoinPrice] = useState(null);
+  let bitcoinSocket = new WebSocket(
+    'wss://stream.binance.com:9443/ws/btcusdt@trade'
+  );
 
-    fetch('https://api.binance.us/api/v3/ticker/price?symbol=BTCUSD').then(
-      // fetch('https://api.binance.us/api/v3/trades?symbol=ETHUSD').then(
-      (res) => {
-        if (res.ok) {
-          res.json().then((data) => {
-            console.log('Bitcoin price on Binance: ' + data.price);
-            setBinancePrice(data.price);
-          });
-        }
-      }
-    );
-  }, []);
+  bitcoinSocket.onmessage = function (event) {
+    console.log(event.data);
+    let messageObject = JSON.parse(event.data);
+    setBitcoinPrice(messageObject.p);
+  };
 
   return (
     <div className="main-container">
       <h1>crypto-dice</h1>
       <p>The game to bet on your coin's next transaction</p>
       <br />
-      {<p>Bitcoin price on Coinbase: {coinbasePrice}</p>}
-      {<p>Bitcoin price on Binance: {binancePrice}</p>}
+      {<p>Bitcoin: {bitcoinPrice}</p>}
+      <br />
     </div>
   );
 }
