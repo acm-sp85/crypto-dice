7c33940410a6afc4d5af1ee8619f38f42db76fd0
Jamie MacKillop <jamiemackillop@Jamies-MacBook-Air.local>
2022-03-16 18:37:21 -0500

    move websocket logic into own component. add lodash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import { useEffect, useState } from 'react';
-
+import BitcoinPrice from './components/BitcoinPrice';
 import './App.css';
 
 function App() {
-  let [bitcoinPrice, setBitcoinPrice] = useState(null);
-  let bitcoinSocket = new WebSocket(
-    'wss://stream.binance.com:9443/ws/btcusdt@trade'
-  );
-
-  bitcoinSocket.onmessage = function (event) {
-    console.log(event.data);
-    let messageObject = JSON.parse(event.data);
-    setBitcoinPrice(messageObject.p);
-  };
-
   return (
     <div className="main-container">
       <h1>crypto-dice</h1>
       <p>The game to bet on your coin's next transaction</p>
       <br />
-      {<p>Bitcoin: ${parseFloat(bitcoinPrice).toFixed(2)}</p>}
+        <BitcoinPrice />
       <br />
     </div>
   );
