1e1beedf049dc1914f73b87a304b4ec6e3b2de84
Alex-new-laptop <acontell@gmail.com>
2022-03-21 10:22:05 -0400

    Added new state on App to keep track of currency price

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import BitcoinPrice from './components/BitcoinPrice';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //adding a piece of state with the live price of our currency
+  const [livePrice, setLivePrice] = useState(null);
   return (
     <div className="main-container">
       <h1>crypto-dice</h1>
       <p>The game to bet on your coin's next transaction</p>
       <br />
-        <BitcoinPrice />
+      {/* passing setLivePrice as a prop to our BitcoinPrice component */}
+      <BitcoinPrice setLivePrice={setLivePrice} />
+      <br />
+      <div>
+        <button>set reference</button>
+        <button>will go up</button>
+        <button>will go down</button>
+      </div>
+      <button>check!</button>
       <br />
     </div>
   );
--- src/components/BitcoinPrice.js
+++ src/components/BitcoinPrice.js
@@ -1,52 +1,54 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-const BitcoinPrice = () => {
-    const [bitcoinPrice, setBitcoinPrice] = useState(null);
-    const [socketError, setSocketError] = useState(null)
+const BitcoinPrice = (props) => {
+  const [bitcoinPrice, setBitcoinPrice] = useState(null);
+  const [socketError, setSocketError] = useState(null);
 
-    useEffect(() => {
-        // initialize bitcoin web socket
-        const bitcoinSocket = new WebSocket(
-          'wss://stream.binance.com:9443/ws/btcusdt@trade'
-        );
+  useEffect(() => {
+    // initialize bitcoin web socket
+    const bitcoinSocket = new WebSocket(
+      'wss://stream.binance.com:9443/ws/btcusdt@trade'
+    );
 
-        // set price on message
-        bitcoinSocket.onmessage = function (event) {
-          let messageObject = JSON.parse(event.data);
-          setBitcoinPrice(messageObject.p);
-        };
-        
-        // handle error state
-        const handleSocketError = (event) => {
-          if (event.message) {
-            setSocketError(event.message)
-          }
-        }
-        bitcoinSocket.addEventListener('error', handleSocketError);
+    // set price on message
+    bitcoinSocket.onmessage = function (event) {
+      let messageObject = JSON.parse(event.data);
+      setBitcoinPrice(messageObject.p);
+      // bubbling up the price to our App's state
+      props.setLivePrice(messageObject.p);
+    };
 
-        // close socket/remove listener when this component unmounts
-        // to avoid memory leaks
-        return () => {
-          bitcoinSocket.removeEventListener('error', handleSocketError)
-          bitcoinSocket.close()
-        }
-    }, [])
+    // handle error state
+    const handleSocketError = (event) => {
+      if (event.message) {
+        setSocketError(event.message);
+      }
+    };
+    bitcoinSocket.addEventListener('error', handleSocketError);
 
-    // if no error or data, assume it's loading
-    if (!socketError && !bitcoinPrice) { 
-      return <div style={{color: 'white'}}>Loading ...</div>
-    }
+    // close socket/remove listener when this component unmounts
+    // to avoid memory leaks
+    return () => {
+      bitcoinSocket.removeEventListener('error', handleSocketError);
+      bitcoinSocket.close();
+    };
+  }, []);
 
-    if (socketError) {
-      return <div style={{color: 'white'}}>Error: {socketError}</div>
-    }
+  // if no error or data, assume it's loading
+  if (!socketError && !bitcoinPrice) {
+    return <div style={{ color: 'white' }}>Loading ...</div>;
+  }
 
-    const formattedCurrency = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(bitcoinPrice)
+  if (socketError) {
+    return <div style={{ color: 'white' }}>Error: {socketError}</div>;
+  }
 
-    return (
-      <div style={{color: 'white'}}>{formattedCurrency}</div>
-    )
+  const formattedCurrency = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(bitcoinPrice);
 
-}
+  return <div style={{ color: 'white' }}>{formattedCurrency}</div>;
+};
 
-export default BitcoinPrice
+export default BitcoinPrice;
