c0b524582250a25b371ff17892e10f711e51fb23
Alex-new-laptop <acontell@gmail.com>
2022-03-21 11:01:06 -0400

    Added new pieces of state on App, also added game logic, it has bugs at the moment

--- src/App.js
+++ src/App.js
@@ -3,8 +3,38 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  //adding a piece of state with the live price of our currency
+  //adding pieces of state with the LivePrice of our currency, our Reference selection, our BetBehaviour and the NextPrice
   const [livePrice, setLivePrice] = useState(null);
+  const [referencePrice, setReferencePrice] = useState(null);
+  const [betType, setBetType] = useState(false);
+  const [nextPrice, setNextPrice] = useState(null);
+
+  // instantiating our currency formatter
+  let formattedCurrency = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(livePrice);
+
+  // function to setReferencePrice when button onClick
+  const selectReference = () => {
+    setReferencePrice(formattedCurrency);
+    console.log('reference: ' + formattedCurrency);
+  };
+
+  // our game logic. It is buggy at the time, it seems to be one step behind
+  const checkingPrice = () => {
+    setNextPrice(formattedCurrency);
+    console.log(formattedCurrency);
+    if (referencePrice > nextPrice && betType) {
+      console.log('You loose');
+    } else if (referencePrice < nextPrice && !betType) {
+      console.log('You loose');
+    } else if (referencePrice === nextPrice) {
+      console.log("It's a tie, try again");
+    } else {
+      console.log('You win');
+    }
+  };
   return (
     <div className="main-container">
       <h1>crypto-dice</h1>
@@ -14,11 +44,12 @@ function App() {
       <BitcoinPrice setLivePrice={setLivePrice} />
       <br />
       <div>
-        <button>set reference</button>
-        <button>will go up</button>
-        <button>will go down</button>
+        <button onClick={selectReference}>set reference</button>
+        {/*  updating our state deppending on button pressed*/}
+        <button onClick={() => setBetType(true)}>will go up</button>
+        <button onClick={() => setBetType(false)}>will go down</button>
       </div>
-      <button>check!</button>
+      <button onClick={checkingPrice}>check!</button>
       <br />
     </div>
   );
